b472a4ba9563f925a8650498389cbbb17fcf6483
moonintheroom <moonintheroom@github.com>
2025-03-25 18:04:59 -0700

    close #6 시리얼 통신 붙일 코드

--- sketch_mar20a.ino
+++ sketch_mar20a.ino
@@ -1,10 +1,30 @@
+String command = "";
+
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT); // STM32 보드에서는 LED_BUILTIN = PA5
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+  while (Serial.available() > 0) {
+    char c = Serial.read();
+    if (c == '\n') {
+      processCommand(command);
+      command = "";
+    } else {
+      command += c;
+    }
+  }
+}
+
+void processCommand(String cmd) {
+  cmd.trim();
+  if (cmd == "ON") {
+    for (int i = 0; i < 5; i++) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(200);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(200);
+    }
+  }
 }
